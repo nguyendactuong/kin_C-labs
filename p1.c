ba18955350d6e6b91fb863bf23512ba14374b27a
nguyendactuong <59680339+nguyendactuong@users.noreply.github.com>
2020-01-09 13:48:40 +0700

    Create p1.c

---
+++ p1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+SIN = 193 311 127
+1
+number alternates 9 3 1 2 9 3 1 2 = 18 6 2 4
+1 + 8 + 6 + 2 + 4 =21
+other alternates: 1 3 1 1
+1 + 3  + 1 + 1 = 6
+21 + 6 = 27
+30 > 27
+30 - 27 = 3
+*/
+/*
+%d = decimal
+%s = string
+scanf("%d", &n);
+*/
+int main(int argc, char* argv[]) {
+  int n = 10;
+  for(int i= 1; i < 4; i++) {
+    scanf("%d", &n);
+  }
+  printf ("enter:%d\n" i*n);
+}
